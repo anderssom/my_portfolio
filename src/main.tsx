6c9449c013eda9ae40f019ef626cd7e82034fc1f
anderssom-saopaulo <anderssom05@gmail.com>
2024-10-29 07:08:37 -0300

    delet: app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import theme from './theme.ts'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/Home/Home.tsx';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
