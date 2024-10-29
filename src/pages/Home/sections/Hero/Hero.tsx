b9fce6e4842f42f9d7bafeeffbc093783c32db15
anderssom-saopaulo <anderssom05@gmail.com>
2024-10-29 04:33:30 -0300

    erro grid

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid2, Typography, styled } from "@mui/material";
+import { Box, Container, Grid, Typography, styled } from "@mui/material";
 import Avatar from "../../../../assets/images/Avatar.jpeg";
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
@@ -31,8 +31,8 @@ const Hero = () => {
         <>
             <StyledHero>
                 <Container maxWidth="lg">
-                    <Grid2 container spacing={2}>
-                        <Grid2 item xs={12} md={5}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={5}>
                             <Box position="relative">
                                 <Box position="absolute" width={"150%"} top={-100} right={0}>
                                     <AnimatedBackground />
@@ -41,30 +41,30 @@ const Hero = () => {
                                     <StyledImg src={Avatar} />
                                 </Box>
                             </Box>
-                        </Grid2>
-                        <Grid2 item xs={12} md={7}>
+                        </Grid>
+                        <Grid item xs={12} md={7}>
                             <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Anderson Silva</Typography>
                             <Typography color="primary.contrastText" variant="h2" textAlign="center" >I'm a Develop Software</Typography>
-                            <Grid2 container display="flex" justifyContent="center" spacing={3} pt={3}>
-                                <Grid2 item xs={12} md={4} display="flex" justifyContent="center">
+                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
                                     <StyledButton>
                                         <DownloadIcon />
                                         <Typography>
                                             Download CV
                                         </Typography>
                                     </StyledButton>
-                                </Grid2>
-                                <Grid2 item xs={12} md={4} display="flex" justifyContent="center">
+                                </Grid>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
                                     <StyledButton>
                                         <MailOutlineIcon />
                                         <Typography>
                                             Contact me
                                         </Typography>
                                     </StyledButton>
-                                </Grid2>
-                            </Grid2>
-                        </Grid2>
-                    </Grid2>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Container>
             </StyledHero>
         </>
